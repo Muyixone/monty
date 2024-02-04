72bd6bc291089be9ea19d59ef1822dc9644e1905
Osamuyi <osamuyia1@gmail.com>
2024-02-04 11:16:54 +0100

    Created the error_not_valid function

--- errors.c
+++ errors.c
@@ -11,3 +11,17 @@ void malloc_error(void)
 		free(args_params);
 		exit(EXIT_FAILURE);
 }
+
+/**
+  * instruction_not_valid - Throws error when an instruction is not valid
+*/
+
+void instruction_not_valid(void)
+{
+	dprintf(2, "L%d: unknown instruction %s\n", args_params->string_line_num,
+			args_params->token[0]);
+	stream_end();
+	free_token();
+	free_args();
+	exit(EXIT_FAILURE);
+}
